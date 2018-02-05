7a439a1a895a68b38989902e980ba8a228058abd
Weverton <weverton.cardoso@resourceit.com>
2018-02-05 16:51:49 -0200

    datePicker

--- app/src/main/java/fiap/com/br/finanask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/fiap/com/br/finanask/ui/activity/ListaTransacoesActivity.kt
@@ -1,15 +1,22 @@
 package fiap.com.br.finanask.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import fiap.com.br.finanask.R
+import fiap.com.br.finanask.extension.formataBrasileiro
 import fiap.com.br.finanask.model.Tipo
 import fiap.com.br.finanask.model.Transacao
 import fiap.com.br.finanask.ui.ResumoView
 import fiap.com.br.finanask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
+import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
@@ -25,8 +32,33 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         lista_transacoes_adiciona_receita
                 .setOnClickListener {
-                    Toast.makeText(this@ListaTransacoesActivity,
-                            "clique de receita", Toast.LENGTH_LONG).show();
+                    val view: View = window.decorView
+                    val viewCriada = LayoutInflater.from(this)
+                            .inflate(R.layout.form_transacao, view as ViewGroup, false)
+
+                    val ano = 2018
+                    val mes = 1
+                    val dia = 5
+
+                    val hoje = Calendar.getInstance()
+                    viewCriada.form_transacao_data.setText(hoje.formataBrasileiro())
+
+                    viewCriada.form_transacao_data
+                            .setOnClickListener {
+                                DatePickerDialog(this,
+                                        DatePickerDialog.OnDateSetListener { view, ano, mes, dia ->
+                                            val dataSelecionada = Calendar.getInstance()
+                                            dataSelecionada.set(ano, mes, dia)
+                                            viewCriada.form_transacao_data
+                                                    .setText(dataSelecionada.formataBrasileiro())
+                                        }, ano, mes, dia).show()
+                            }
+
+                    AlertDialog.Builder(this)
+                            .setTitle(R.string.adiciona_receita)
+                            .setView(viewCriada)
+                            .show()
+
                 }
     }
 
