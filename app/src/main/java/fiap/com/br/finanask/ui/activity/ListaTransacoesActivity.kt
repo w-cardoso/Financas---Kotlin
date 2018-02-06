4a662c55e7ca2264377e1299b3c3ac8a694f8b28
Weverton <weverton.cardoso@resourceit.com>
2018-02-06 16:10:44 -0200

    list mutavel

--- app/src/main/java/fiap/com/br/finanask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/fiap/com/br/finanask/ui/activity/ListaTransacoesActivity.kt
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import fiap.com.br.finanask.R
 import fiap.com.br.finanask.extension.formataBrasileiro
 import fiap.com.br.finanask.model.Tipo
@@ -16,19 +18,23 @@ import fiap.com.br.finanask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
+
+    private val transacoes: MutableList<Transacao> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = transacoesDeExemplo()
 
-        configuraResumo(transacoes)
 
-        configuraLista(transacoes)
+        configuraResumo()
+
+        configuraLista()
 
         lista_transacoes_adiciona_receita
                 .setOnClickListener {
@@ -50,33 +56,70 @@ class ListaTransacoesActivity : AppCompatActivity() {
                                             val dataSelecionada = Calendar.getInstance()
                                             dataSelecionada.set(ano, mes, dia)
                                             viewCriada.form_transacao_data
+
                                                     .setText(dataSelecionada.formataBrasileiro())
                                         }, ano, mes, dia).show()
                             }
 
+                    val adapter = ArrayAdapter
+                            .createFromResource(this,
+                                    R.array.categorias_de_receita,
+                                    android.R.layout.simple_spinner_dropdown_item)
+
+                    viewCriada.form_transacao_categoria.adapter = adapter
+
+
                     AlertDialog.Builder(this)
                             .setTitle(R.string.adiciona_receita)
                             .setView(viewCriada)
+                            .setPositiveButton("Adiconar", { dialoginterface, which ->
+
+                                val valorEmTexto = viewCriada.form_transacao_valor.text.toString()
+                                val dataEmTexto = viewCriada.form_transacao_data.text.toString()
+                                val categoriaEmTexto = viewCriada.form_transacao_categoria.selectedItem.toString()
+
+                                val valor = try {
+                                    BigDecimal(valorEmTexto)
+                                } catch (exception: NumberFormatException) {
+                                    Toast.makeText(this, "Falha na conversão de valor", Toast.LENGTH_LONG).show()
+                                    BigDecimal.ZERO
+                                }
+
+                                val formatoBrasileiro = SimpleDateFormat("dd/MM/yyyy")
+                                val dataCovertida: Date = formatoBrasileiro.parse(dataEmTexto)
+                                val data = Calendar.getInstance()
+                                data.time = dataCovertida
+
+                                val transacaoCriada = Transacao(tipo = Tipo.RECEITA,
+                                        valor = valor,
+                                        data = data,
+                                        categoria = categoriaEmTexto)
+
+                                atualizaTransacoes(transacaoCriada)
+                                lista_transacoes_adiciona_menu.close(true)
+
+
+                            })
+                            .setNegativeButton("Cancelar", null)
                             .show()
 
                 }
     }
 
-    private fun configuraResumo(transacoes: List<Transacao>) {
+    private fun atualizaTransacoes(transacao: Transacao) {
+        transacoes.add(transacao)
+        configuraLista()
+        configuraResumo()
+    }
+
+    private fun configuraResumo() {
         val view = window.decorView
         val resumoView = ResumoView(view, this, transacoes)
         resumoView.atualiza()
     }
 
-    private fun transacoesDeExemplo(): List<Transacao> {
-        return listOf(
-                Transacao(valor = BigDecimal(100.0), categoria = "Economia Baseada em Bitcoin", tipo = Tipo.RECEITA),
-                Transacao(valor = BigDecimal(300.0), categoria = "Comida", tipo = Tipo.DESPESA),
-                Transacao(valor = BigDecimal(100.0), categoria = "Economia Baseada em Bitcoin", tipo = Tipo.RECEITA),
-                Transacao(valor = BigDecimal(300.0), categoria = "Comida", tipo = Tipo.DESPESA))
-    }
 
-    private fun configuraLista(transacoes: List<Transacao>) {
+    private fun configuraLista() {
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
     }
 }
